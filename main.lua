f72a3f542a2e0dbf974fbcc35bf00552638b7e22
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-04 15:41:59 +0100

    Added simple screen edge collision detection

--- main.lua
+++ main.lua
@@ -1,27 +1,26 @@
 
 local ENTITIES =
 {
-    [1] = { position = true, velocity = true },
-    [2] = { position = true, velocity = true },
-    [3] = { position = true, velocity = true }
+    [1] = { position = true, velocity = true, collision = true },
+    [2] = { position = true, velocity = true, collision = true },
+    [3] = { position = true, velocity = true, collision = true },
 }
 
 local COMPONENTS =
 {
     position =
     {
-        [1] = { x = 40, y = 40},
+        [1] = { x = 340, y = 40},
         [2] = { x = 140, y = 440},
         [3] = { x = 240, y = 330},
     },
 
     velocity =
     {
-        [1] = { max_speed = 10, x = 20,  y = 30,  friction = 0.5, },
-        [2] = { max_speed = 30, x = 10,  y = 10,  friction = 3,   },
-        [3] = { max_speed = 30, x = -30, y = -20, friction = 3,   }
+        [1] = { max_speed = 10, x = 220,  y = 230,  friction = 0.5, },
+        [2] = { max_speed = 30, x = 210,  y = 110,  friction = 3,   },
+        [3] = { max_speed = 30, x = -230, y = -20, friction = -3,   }
     },
-
 }
 
 function hasComponent(entity_id, component)
@@ -41,8 +40,17 @@ function love.update(dt)
         --print(entity_id)
         local position = COMPONENTS["position"][entity_id]
 
-        position.x = position.x + velocity.x * dt
-        position.y = position.y + velocity.y * dt
+        position.x = position.x + velocity.x *  velocity.friction * dt
+        position.y = position.y + velocity.y *  velocity.friction * dt
+
+        if(hasComponent(entity_id, "collision"))then
+            if(position.x < 0 or position.x > 800)then
+                velocity.x = -velocity.x;
+            end
+            if(position.y < 0 or position.y > 600)then
+                velocity.y = -velocity.y;
+            end
+        end
 
     end
     --
