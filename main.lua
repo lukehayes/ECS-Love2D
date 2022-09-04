40f3875d28330ebbe6ccdbd0d8ccc05c496346be
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-04 18:17:44 +0100

    Basic idea of tiny-ecs is now working.

--- main.lua
+++ main.lua
@@ -1,58 +1,34 @@
+local tiny = require("libs/tiny")
+local game = {}
+local system = {}
 
-local ENTITIES =
-{
-    [1] = {
-        position = { x = 340, y = 40},
-        velocity = { x = 220, y = 230},
-        color    = {0,0,200}
-    },
-
-    [2] = {
-        position  = { x = 40, y = 240},
-        velocity  = { x = 2, y = 23},
-        color    = {0,200,0}
-    },
+local e1 = {
+    position = {x = 100,y = 100}
 }
 
-for i = 1,10,1 do
-    local e = {
-        position = {x = math.random(0,500), y = math.random(0,500)},
-        color = {math.random(255,255), math.random(0,255), math.random(0,255)}
-    }
+local e2 = {
+    position = {x = 400,y = 200}
+}
 
-    if(math.random(0,1) == 1) then
-        local velocity = {x = math.random(-800,800), y = math.random(-600,600)}
-        e.velocity = velocity
-    end
-
-    table.insert(ENTITIES, e)
+system.draw = tiny.processingSystem()
+system.draw.filter = tiny.requireAll("position")
+function system.draw:process(entity)
+    love.graphics.rectangle("fill", entity.position.x, entity.position.y, 10,10)
 end
 
-
-function hasComponent(entity_id, component)
-    local entity = ENTITIES[entity_id]
-    return entity[component]
+function love.load()
+    game.world = tiny.world(
+    e1,
+    e2,
+    system.draw
+    )
 end
+
 
 function love.update(dt)
-
-    for id, entity  in pairs(ENTITIES) do
-        if(hasComponent(id, "velocity")) then
-            entity.position.x = entity.position.x + entity.velocity.x *  dt
-            entity.position.y = entity.position.y + entity.velocity.y *  dt
-        end
-    end
+    game.world:update(dt)
+end
+function love.draw(dt)
+    print(system.draw:update(dt))
 end
 
-function love.draw()
-    for id, entity in pairs(ENTITIES) do
-        if(hasComponent(id, "position")) then
-            love.graphics.rectangle("fill",
-            entity.position.x,
-            entity.position.y,
-            10,
-            10
-            )
-        end
-    end
-end
