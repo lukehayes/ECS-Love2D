cdfa4653e87d2e9d5694cc43d9e05ac3b01d46fb
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-05 15:44:58 +0100

    Added acceleration and max_speed value to velocity component.

--- components/velocity.lua
+++ components/velocity.lua
@@ -1,5 +1,10 @@
-function velocity(x,y)
-    return {x = x, y = y}
+function velocity(x,y, acceleration, max_speed)
+    return {
+        x = x,
+        y = y,
+        acceleration = acceleration,
+        max_speed = max_speed
+    }
 end
 
 return velocity
--- main.lua
+++ main.lua
@@ -6,18 +6,33 @@ local velocity = require("components/velocity")
 
 local e1 = {
     position = position(100,100),
-    velocity = velocity(10,20),
+    velocity = velocity(10,20, 0.5, 30),
 }
 
 local e2 = {
     position = position(400,200),
-    velocity = velocity(10,-20)
+    velocity = velocity(10,-20, 0.9,30)
 }
 
 
 system.movement = tiny.processingSystem()
 system.movement.filter = tiny.requireAll("velocity", "position")
 function system.movement:process(entity, dt)
+
+    entity.velocity.x  = entity.velocity.x + entity.velocity.acceleration
+    entity.velocity.y  = entity.velocity.y + entity.velocity.acceleration
+
+    if(entity.velocity.x >= entity.velocity.max_speed) then
+        entity.velocity.x = entity.velocity.max_speed
+    end
+
+    if(entity.velocity.y >= entity.velocity.max_speed) then
+        --entity.velocity.y = entity.velocity.max_speed
+        entity.velocity.y = -entity.velocity.y
+    end
+
+    print(entity.velocity.x)
+
     entity.position.x = entity.position.x + entity.velocity.x * dt
     entity.position.y = entity.position.y + entity.velocity.y * dt
 end
@@ -35,6 +50,15 @@ function love.load()
 
     tiny.addEntity(game.world, e1)
     tiny.addEntity(game.world, e2)
+
+    for i = 1,100,1 do
+        local e = {
+            position = position(math.random(0,500), math.random(0,500)),
+            velocity = velocity(-40,-20, 0.9, math.random(100,200))
+        }
+
+        tiny.addEntity(game.world, e)
+    end
 end
 
 function love.update(dt)
