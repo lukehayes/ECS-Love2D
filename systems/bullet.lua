4165f640d9f86db8911b4195190131b975c1854f
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-07 18:00:48 +0100

    Added bullet entity out of bounds check.

--- systems/bullet.lua
+++ systems/bullet.lua
@@ -1,6 +1,7 @@
 local tiny = require("libs/tiny")
 local system = require("systems/system")
 local mathUtils = require("libs/math")
+local game   = require("core/game")
 
 system.bullet = tiny.processingSystem()
 system.bullet.filter = tiny.requireAll("bullet", "position", "velocity")
@@ -14,6 +15,13 @@ function system.bullet:process(entity, dt)
         )
     entity.position.x = entity.position.x + math.cos(angle) + entity.velocity.acceleration * dt
     entity.position.y = entity.position.y + math.sin(angle) + entity.velocity.acceleration * dt
+
+    -- Remove out of bounds bullets
+    if(entity.position.x < 0 or entity.position.x >= (600) - 20 ) then
+
+        -- FIX Bullets should be removed but isn't'working for some reason.
+        self.world:removeEntity(game.world, entity)
+    end
 end
 
 return system
