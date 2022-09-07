b2e8c6847c58869af761fc7f4c5e522a3e1c8bb4
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-07 09:21:10 +0100

    Changed base entities default acceleration value.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-local tiny = require("libs/tiny")
+local tiny      = require("libs/tiny")
 local mathUtils = require("libs/math")
-local game = require("core/game")
+local game      = require("core/game")
 
 -- Components
 local position = require("components/position")
@@ -14,18 +14,16 @@ local movementSystem = require("systems/movement")
 local shooterSystem  = require("systems/shooter")
 local bulletSystem   = require("systems/bullet")
 
-print(system)
-
 local e1 = {
     position = position(100,100),
-    velocity = velocity(10,20, 10, 30),
+    velocity = velocity(0,0, 0.5, 1),
     size     = size(10,10),
     shooter  = shooter(2, 2)
 }
 
 local e2 = {
     position = position(400,200),
-    velocity = velocity(10,-20, 20,10),
+    velocity = velocity(0,0, 0.5, 1),
     size     = size(10,10),
 }
 
@@ -40,19 +38,19 @@ function love.load()
     tiny.addEntity(game.world, e1)
     tiny.addEntity(game.world, e2)
 
-    --for i = 1,100,1 do
-        --local e = {
-            --position = position(100 + math.random(0,500), 100 + math.random(0,500)),
-            --velocity = velocity(-40,-20, math.random(10,100), math.random(100,200)),
-            --size     = size(math.random(5,20), math.random(5,20))
-        --}
+    for i = 1,10,1 do
+        local e = {
+            position = position(100 + math.random(0,500), 100 + math.random(0,500)),
+            velocity = velocity(0,0, math.random(0.2,1), math.random(2,5)),
+            size     = size(10,10)
+        }
 
-        --if(math.random(0,1) == 1) then
-            --e.shooter = shooter(math.random(1.0, 5.0), 2)
-        --end
+        if(math.random(0,1) == 1) then
+            e.shooter = shooter(math.random(1.0, 5.0), 2)
+        end
 
-        --tiny.addEntity(game.world, e)
-    --end
+        tiny.addEntity(game.world, e)
+    end
 end
 
 function love.update(dt)
