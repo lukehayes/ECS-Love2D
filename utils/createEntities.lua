3b77140b79229a025b53900a0d58e28a85197ddc
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-07 18:01:31 +0100

    Moved test entity creation to utils/createEntities.lua

---
+++ utils/createEntities.lua
@@ -0,0 +1,26 @@
+local tiny      = require("libs/tiny")
+local game      = require("core/game")
+local sound    = require("components/sound")
+
+function createTestEntities(count)
+
+    assert(type(count) == "number", "count argument should be a number")
+
+    for i = 1,count,1 do
+        local e = {
+            position = position(100 + math.random(0,500), 100 + math.random(0,500)),
+            velocity = velocity(0,0, math.random(0.2,1), math.random(2,5)),
+            size     = size(10,10),
+            sound    = {
+                jump = sound("assets/sounds/powerUp.mp3"),
+                fire = sound("assets/sounds/laser.mp3")
+            }
+        }
+
+        if(math.random(0,1) == 1) then
+            e.shooter = shooter(math.random(5.0, 10.0), 2)
+        end
+
+        tiny.addEntity(game.world, e)
+    end
+end
