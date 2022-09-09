a568f502039f5fe9389a29ddfd58832908bc065a
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-09 10:31:43 +0100

    Added color and sprite component to createEntities function.

--- systems/draw.lua
+++ systems/draw.lua
@@ -10,7 +10,13 @@ function system.draw:process(entity)
 
     if(entity.color)then
         love.graphics.setColor(entity.color.r, entity.color.g, entity.color.b)
-        love.graphics.rectangle("fill", entity.position.x, entity.position.y, 1,1)
+        love.graphics.rectangle(
+        "fill", 
+        entity.position.x, 
+        entity.position.y,
+        entity.size.w,
+        entity.size.h
+        )
         --love.graphics.print("Pew", entity.position.x + 5, entity.position.y + 5)
     else
         love.graphics.setColor(1,1,1, 0.5)
--- utils/createEntities.lua
+++ utils/createEntities.lua
@@ -11,10 +11,16 @@ function createTestEntities(count)
             position = position(100 + math.random(0,500), 100 + math.random(0,500)),
             velocity = velocity(0,0, math.random(0.2,1), math.random(2,5)),
             size     = size(10,10),
+            color = {
+                r = math.random(0.5, 1.0),
+                g = math.random(0.0, 1.0),
+                b = math.random(0.6, 1.0)
+            },
             sound    = {
                 jump = sound("assets/sounds/powerUp.mp3"),
                 fire = sound("assets/sounds/laser.mp3")
-            }
+            },
+            sprite = true
         }
 
         if(math.random(0,1) == 1) then
