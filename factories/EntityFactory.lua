28b2c4a71a5f5508ad4dad4109bb7bd90af75ed5
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-19 17:31:28 +0100

    Added factories.EntityFactory to project.

---
+++ factories/EntityFactory.lua
@@ -0,0 +1,33 @@
+local Position = require('components.Position')
+local Velocity = require('components.Velocity')
+local Color    = require('components.Color')
+
+function generate(count)
+    local entities = {}
+
+    for i = 1,count do
+        local ent = Entity()
+
+        ent:add(Position(
+            math.random(10,400),
+            math.random(10,500))
+        )
+
+        ent:add(Velocity(
+            math.random(-600,600),
+            math.random(-600,600)
+        ))
+
+        ent:add(Color(
+            math.random(0.0,0.0),
+            math.random(0.4,1.0),
+            math.random(0.6,1.0)
+        ))
+
+        --table.insert(entities, ent)
+        engine:addEntity(ent)
+    end
+
+    return entities
+end
+
