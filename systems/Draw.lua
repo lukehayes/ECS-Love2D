3719278b6a324116f4b12568fef50df63172c972
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-19 17:32:01 +0100

    Draw system now has an entity size variable.

--- systems/Draw.lua
+++ systems/Draw.lua
@@ -1,5 +1,6 @@
 -- Create a draw System.
 local DrawSystem = class("DrawSystem", System)
+local size = 1
 
 -- Define this System requirements.
 function DrawSystem:requires()
@@ -15,7 +16,7 @@ function DrawSystem:draw()
             entity:get("color").b
         )
 
-        love.graphics.rectangle("fill", entity:get("position").x, entity:get("position").y, 10, 10)
+        love.graphics.rectangle("fill", entity:get("position").x, entity:get("position").y, size, size)
     end
 end
 
