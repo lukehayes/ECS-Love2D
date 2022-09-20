e0b69ba1754572330350b1f9c40738aa38c2f379
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-20 16:24:23 +0100

    Added local position variable for ease.

--- systems/Draw.lua
+++ systems/Draw.lua
@@ -1,6 +1,6 @@
 -- Create a draw System.
 local DrawSystem = class("DrawSystem", System)
-local size = 1
+local size = 5
 
 -- Define this System requirements.
 function DrawSystem:requires()
@@ -10,13 +10,15 @@ end
 function DrawSystem:draw()
     for _, entity in pairs(self.targets) do
 
+        local position = entity:get("position")
+
         love.graphics.setColor(
             entity:get("color").r,
             entity:get("color").g,
             entity:get("color").b
         )
 
-        love.graphics.rectangle("fill", entity:get("position").x, entity:get("position").y, size, size)
+        love.graphics.rectangle("fill", position.x, position.y, size, size)
     end
 end
 
