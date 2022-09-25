23f77cc0aefcb9f8e1110ed1edc2e33272edb388
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-25 17:11:53 +0100

    Added start of new player movement to move system.

--- systems/Move.lua
+++ systems/Move.lua
@@ -20,20 +20,32 @@ function MoveSystem:update(dt)
         -- 
         if(entity:has("player")) then
             if love.keyboard.isDown('a') then
-                position.x = position.x + velocity.vx * dt
+                velocity.vx = velocity.vx + velocity.acceleration * dt
+            else
+                velocity.vx = velocity.vx - velocity.friction * dt
             end
 
             if love.keyboard.isDown('d') then
-                position.x = position.x - velocity.vx * dt
+                velocity.vx = velocity.vx - 10.0 * dt
             end
 
             if love.keyboard.isDown('w') then
-                position.y = position.y + velocity.vy * dt
+                --position.y = position.y + velocity.vy * dt
             end
 
             if love.keyboard.isDown('s') then
-                position.y = position.y - velocity.vy * dt
+                --position.y = position.y - velocity.vy * dt
             end
+
+            if(velocity.vx < 0 ) then
+                velocity.vx = 0
+            end
+
+            position.x = velocity.vx
+            position.y = velocity.vy
+
+            print(velocity.vx)
+
         else
             position.x = position.x + velocity.vx * dt
             position.y = position.y + velocity.vy * dt
@@ -41,13 +53,13 @@ function MoveSystem:update(dt)
 
         -- Bounds Checking
         --
-        if(position.x < 0 or position.x > 790) then
-            velocity.vx = -velocity.vx
-        end
+        --if(position.x < 0 or position.x > 790) then
+            --velocity.vx = -velocity.vx
+        --end
 
-        if(position.y < 0 or position.y > 590) then
-            velocity.vy = -velocity.vy
-        end
+        --if(position.y < 0 or position.y > 590) then
+            --velocity.vy = -velocity.vy
+        --end
     end
 end
 
