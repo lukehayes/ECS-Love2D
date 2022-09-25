2ec498acfd2ee4c0ee51d877f6c07ba9ed8ac6ec
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-25 11:11:33 +0100

    Corrected formatting of Velocity component.

--- components/Velocity.lua
+++ components/Velocity.lua
@@ -1,1 +1,7 @@
-return Component.create("velocity", {"vx", "vy", "solid"}, {vx =0, vy = 0,solid=false})
+return Component.create("velocity",
+{"vx", "vy", "solid"},
+{
+    vx =0,
+    vy = 0,
+    solid=false
+})
