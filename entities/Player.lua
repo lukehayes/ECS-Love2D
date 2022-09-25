a7213ea99acc4ffbbc4fea689b386554a0a2f40c
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-25 11:25:39 +0100

    Player now positioned on the center of the screen.

--- entities/Player.lua
+++ entities/Player.lua
@@ -11,8 +11,8 @@ local player = Entity()
 player:initialize()
 
 -- Add position and velocity components. We are passing custom default values.
-player:add(Position(150, 25))
-player:add(Velocity(300, 300))
+player:add(Position(love.graphics.getWidth()/2, love.graphics.getHeight() / 2))
+player:add(Velocity(-300, -300))
 player:add(Color())
 player:add(Player())
 
