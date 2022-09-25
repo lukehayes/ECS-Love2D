1dbdbebf56ed4623b385e3271748f5ba6bf521ea
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-25 17:11:25 +0100

    Added acceleration and friction to velocity component.

--- components/Velocity.lua
+++ components/Velocity.lua
@@ -1,7 +1,9 @@
 return Component.create("velocity",
-{"vx", "vy", "solid"},
+{"vx", "vy", "solid", "acceleration", "friction"},
 {
     vx =0,
     vy = 0,
-    solid=false
+    solid=false,
+    acceleration = 20,
+    friction = 10,
 })
