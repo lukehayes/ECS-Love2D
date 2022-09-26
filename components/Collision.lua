7db96eb9b7202ebae3e7ea1a1aea4dd9359b09f4
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-26 10:51:52 +0100

    Changed has_hit property to hit.

--- components/Collision.lua
+++ components/Collision.lua
@@ -1,7 +1,7 @@
 return Component.create("collision",
-{"bound_x", "bound_y", "has_hit"},
+{"bound_x", "bound_y", "hit"},
 {
     bound_x = 16,
     bound_y = 16,
-    has_hit = false
+    hit = false
 })
