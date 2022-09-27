fb95d2ae2c6811f11f164f901bdccb4d7610a58a
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-27 15:15:17 +0100

    Removed old velocity friction code from movement system.

--- systems/Move.lua
+++ systems/Move.lua
@@ -21,10 +21,7 @@ function MoveSystem:update(dt)
         if(entity:has("player")) then
 
             if love.keyboard.isDown('d') then
-
-                if not velocity.vx > velocity.mx then
-                    velocity.vx = velocity.vx + velocity.ax
-                end
+                velocity.vx = velocity.vx + velocity.ax
 
             elseif love.keyboard.isDown('a') then
                 velocity.vx = velocity.vx - velocity.ax
@@ -36,35 +33,13 @@ function MoveSystem:update(dt)
                 velocity.vy = velocity.vy - velocity.ay
 
             else
-
-                if velocity.vx >=  (-velocity.mx) then
-                    velocity.vx = velocity.vx - velocity.drag
-                else
-                    velocity.vx = 0
-                end
-
-                if velocity.vx >=  velocity.mx then
-                    velocity.vx = velocity.mx
-                else
-                    velocity.vx = velocity.vx - velocity.drag
-                end
-
-                if velocity.vy <=  (-velocity.my) then
-                    velocity.vy = velocity.vy - velocity.drag
-                else
-                    velocity.vy = 0
-                end
-
-                if velocity.vy >=  velocity.my then
-                    velocity.vy = velocity.my
-                else
-                    velocity.vy = velocity.vy - velocity.drag
-                end
-
-                --velocity.vy = velocity.vy - velocity.drag
+                -- TODO Friction drag code.
+                -- Empty else statement is working fine for now.
+                --
             end
 
                 print(velocity.vx, velocity.mx)
+                print(velocity.vy, velocity.my)
 
             position.x = position.x + velocity.vx * dt
             position.y = position.y + velocity.vy * dt
