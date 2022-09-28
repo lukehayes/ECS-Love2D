8f4cf7162ec645268b693acf4044e96807da5170
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-28 17:58:21 +0100

    Player velocity constructor now has explicit values.

--- entities/Player.lua
+++ entities/Player.lua
@@ -1,8 +1,9 @@
-local Position = require('components.Position')
-local Player   = require('components.Player')
-local Velocity = require('components.Velocity')
-local Color    = require('components.Color')
-local Collision    = require('components.Collision')
+local Position  = require('components.Position')
+local Player    = require('components.Player')
+local Velocity  = require('components.Velocity')
+local Color     = require('components.Color')
+local Collision = require('components.Collision')
+local Sprite    = require('components.Sprite')
 
 -- Create and initialize a new Entity.
 -- Note we can access Entity() in the global
@@ -11,12 +12,14 @@ local Collision    = require('components.Collision')
 local player = Entity()
 player:initialize()
 
+--local Color = Component.load({"Color"})
+
 -- Add position and velocity components. We are passing custom default values.
 player:add(Position(love.graphics.getWidth()/2, love.graphics.getHeight() / 2))
-player:add(Velocity(0,0))
+player:add(Velocity(0,0, solid, 10,10, 5, 100,100))
 player:add(Color())
 player:add(Collision())
 player:add(Player())
-
+player:add(Sprite("FUCK"))
 
 return player
