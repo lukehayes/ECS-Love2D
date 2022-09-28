5528e7149400f1f5e7ed9f5c16b09ba72a8260c1
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-28 18:00:31 +0100

    Trying to create an actual Sprite class, not there yet.

--- components/Sprite.lua
+++ components/Sprite.lua
@@ -1,5 +1,19 @@
-return Component.create("sprite",
-{"image"},
-{
-    image = love.graphics.newImage("assets/face.png")
-})
+--return Component.create("sprite",
+--{"path", "image"},
+--{
+    --path = path or "assets/face.png",
+    --image = love.graphics.newImage(path)
+--})
+
+
+-- TODO This whole idea doesn't work.
+local lovetoys = require('libs.lovetoys')
+local Sprite = lovetoys.class("Sprite")
+
+function Sprite:intialize(path)
+    print("Sprite initialized")
+    self.path = path
+    --self.image = love.graphics.newImage(self.path)
+end
+
+return Sprite
