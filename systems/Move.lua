67f76e36934f3201037149144e6e13f3dd3f84f7
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-28 10:42:58 +0100

    Finally fixed x axis velocity movement.

--- systems/Move.lua
+++ systems/Move.lua
@@ -21,89 +21,29 @@ function MoveSystem:update(dt)
         -- 
         if(entity:has("player")) then
 
-            if love.keyboard.isDown('a') then
+
+            if(love.keyboard.isDown('a')) then
                 velocity.x = velocity.x - velocity.ax
-            else
 
+            elseif(love.keyboard.isDown('d')) then
                 velocity.x = velocity.x + velocity.ax
 
-                if (velocity.x > -0.4 and velocity.x < 0) or (velocity.x < 0.5 and velocity.x > 0) then
-                    velocity.x = 0
+            else
+                local sign = mathUtil.sign(velocity.x)
+
+                if sign == 1 then
+                    velocity.x = velocity.x - velocity.drag
+                elseif sign == -1 then
+                    velocity.x = velocity.x + velocity.drag
                 end
-            end
-
-            if love.keyboard.isDown('d') then
-                velocity.x = velocity.x + velocity.ax
-            else
-                velocity.x = velocity.x - 1
-
-                --if (velocity.x > -0.4 and velocity.x < 0) or (velocity.x < 0.5 and velocity.x > 0) then
-                    --velocity.x = 0
-                --else
-                    --velocity.x = velocity.x - velocity.ax
-                --end
 
             end
 
-            --if love.keyboard.isDown('d') then
-                --velocity.x = velocity.x + velocity.ax
-            --else
+            --Find the sign (+/-) of velocity and then Velocity = velocity + (const * -sign)
 
-                --if velocity.x < -0.5 or velocity.x > 0.5 then
-                    --velocity.x = 0
-                --end
-            --end
-
-            --if love.keyboard.isDown('d') then
-
-                ---- Increase velocity on key press.
-                --velocity.vx = velocity.vx + velocity.ax
-
-                ---- Stop increasing velocity when max reached
-                --if velocity.vx > velocity.mx then
-                    --velocity.vx = velocity.mx
-                --end
-
-                --keyDown = true
-            --else
-                --velocity.vx = velocity.vx + velocity.drag
-            --end
-
-            --keyDown = false
-
-            --elseif love.keyboard.isDown('a') then
-                --velocity.vx = velocity.vx - velocity.ax
-
-                --if velocity.vx < -velocity.mx then
-                    --velocity.vx = -velocity.mx
-                --end
-
-                --keyDown = true
-                ----velocity.vx = velocity.vx - velocity.drag
-            --elseif love.keyboard.isDown('s') then
-                --velocity.vy = velocity.vy + velocity.ay
-                --keyDown = true
-
-            --elseif love.keyboard.isDown('w') then
-                --velocity.vy = velocity.vy - velocity.ay
-
-            --else
-                --keyDown = false
-                ---- TODO Friction drag code.
-                ---- Empty else statement is working fine for now.
-                ----
-                ----velocity.vx = velocity.vx - velocity.drag
-            --end
-
-
-            --if not keyDown then
-                --velocity.vx = velocity.vx - velocity.drag
-            --end
-
-
-            print("----")
-            print("VX ", velocity.x, velocity.mx)
-            print("VY ", velocity.y, velocity.my)
+            print("--")
+            print("Vx ", velocity.x)
+            print("Vy ", velocity.y)
 
             position.x = position.x + velocity.x * dt
             position.y = position.y + velocity.y * dt
