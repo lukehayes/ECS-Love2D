222d9ebde84a689ef00a6f98362ddbbbc67afdcc
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-30 12:12:19 +0100

    Added fw.io.FileReader class to project.

---
+++ fw/io/FileReader.lua
@@ -0,0 +1,38 @@
+--- Class that reads in file contents
+-- @module fw.io
+--
+local class= require "libs.middleclass"
+local FileReader = class("FileReader")
+
+--- Constructor
+--
+-- @raise Assertion thrown if path is not a string.
+--
+-- @param path The file path to read from
+--
+function FileReader:initialize(path)
+    assert(type(path) == 'string', 'Path is not a string.')
+    self.path = path or "Path not set!"
+end
+
+
+--- Read entire contents of a file into a string.
+--
+-- @raise Assertion thrown if self.path is not a string.
+--
+-- @return string The contents of the file.
+--
+function FileReader:contents()
+
+    assert(type(self.path) == 'string', 'Path is not a string.')
+
+    local fh = io.open(self.path, "r")
+    local contents = fh:read("*all")
+    fh:close()
+
+    return contents
+end
+
+return FileReader
+
+
