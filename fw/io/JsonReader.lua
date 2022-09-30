43290bd75443e791a72e08985db4556e0192371f
Luke Hayes <lukepurplehayes@gmail.com>
2022-09-30 12:22:22 +0100

    Added JsonReader class to project.

---
+++ fw/io/JsonReader.lua
@@ -0,0 +1,15 @@
+--- Class that reads in the contents of a JSON file.
+-- @module fw.io
+--
+local class = require "libs.middleclass"
+local json = require "libs.json"
+local FileReader = require "fw.io.FileReader"
+local JsonReader = class("JsonReader")
+
+function JsonReader:intialize(path)
+    local contents = FileReader:new(path)
+    print(contents)
+    self.json = json.decode(contents:contents())
+end
+
+return JsonReader
