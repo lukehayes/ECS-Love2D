b68b21e7a8c01f1a2113933ad8cae23f9ea575f6
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-01 16:03:17 +0100

    JsonReader now inherits from FileReader class.

--- fw/io/JsonReader.lua
+++ fw/io/JsonReader.lua
@@ -4,12 +4,11 @@
 local class = require "libs.middleclass"
 local json = require "libs.json"
 local FileReader = require "fw.io.FileReader"
-local JsonReader = class("JsonReader")
+local JsonReader = class("JsonReader", FileReader)
 
 function JsonReader:initialize(path)
-    local contents = FileReader:new(path)
-    print(contents)
-    self.json = json.decode(contents:contents())
+    FileReader:initialize(path)
+    self.json = json.decode(self:contents())
 end
 
 return JsonReader
