3d917e3152deabfeff612739d69d796810e238e5
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-02 11:04:46 +0100

    Added JsonReader:getMeta() and JsonReader:getFrameTags() methods.

--- fw/io/JsonReader.lua
+++ fw/io/JsonReader.lua
@@ -11,4 +11,16 @@ function JsonReader:initialize(path)
     self.json = json.decode(self:contents())
 end
 
+--- Get the "meta" table from the JSON Aseprite file.
+--
+function JsonReader:getMeta()
+    return self.json.meta
+end
+
+--- Get the "frameTags" ARRAY from the JSON Aseprite file.
+--
+function JsonReader:getFrameTags()
+    return self.json.meta.frameTags
+end
+
 return JsonReader
