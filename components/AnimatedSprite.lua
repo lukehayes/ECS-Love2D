623e35ab3892c58cdf1a68f1b9d7186702944c89
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-03 17:42:43 +0100

    Added AnimatedSprite:generateQuads() method

--- components/AnimatedSprite.lua
+++ components/AnimatedSprite.lua
@@ -1,13 +1,17 @@
 local lovetoys = require('libs.lovetoys')
 local Sprite = require "components.Sprite"
 local AnimatedSprite = lovetoys.class("AnimatedSprite", Sprite)
+local JsonReader = require "fw.io.JsonReader"
+local JSON = JsonReader:new("assets/Bob.json")
+local Util = require "fw.util.Util"
+local d = require "libs.dump"
 
-function Sprite:initialize(path)
+function AnimatedSprite:initialize(path)
     self.path = path
     self.image = love.graphics.newImage(self.path)
     self.image:setFilter("nearest", "nearest")
 
-    -- TODO Move this table into a mixin.
+     --TODO Move this table into a mixin.
     self.config = {
         rotation = 0,
         scale_x = 2,
@@ -16,19 +20,40 @@ function Sprite:initialize(path)
         offset_y = 0
     }
 
+    self.frameTags = JSON:getFrameTags()
+
     self.anim = {
         size = 16,
         count = 4,
-        duration = 0.1,
-
-        frames =
-        {
-            love.graphics.newQuad(0,0,16,16, self.image),
-            love.graphics.newQuad(16,0,16,16,self.image),
-            love.graphics.newQuad(32,0,16,16,self.image),
-            love.graphics.newQuad(48,0,16,16,self.image)
-        }
+        speed = 0.1,
+        frames = self:generateQuads()
     }
+
 end
 
-return Sprite
+
+--- Generate every Quad needed for each frame of animation.
+--
+function AnimatedSprite:generateQuads()
+
+    local quads = {}
+
+    for k,v in pairs(self.frameTags) do
+
+        local t = quads[v.name] = {}
+
+        for i = v.from, v.to do
+            table.insert(
+                t,
+                love.graphics.newQuad(i * 16,0,16,16, self.image)
+            )
+
+            table.insert(quads[v.name], t)
+        end
+    end
+
+
+    return quads
+end
+
+return AnimatedSprite
