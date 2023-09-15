25343efd2d125484b12bce93819d896746de62b6
lukehayes <lukepurplehayes@gmail.com>
2023-09-15 17:59:09 +0100

    Added test systemDraw() and systemMove() functions.

--- main.lua
+++ main.lua
@@ -17,12 +17,16 @@ local tt = {}
 
 table.insert(components.position,1, PositionComp(100,100))
 table.insert(components.position,2, PositionComp(200,200))
-table.insert(components.position,3, PositionComp(300,400))
+table.insert(components.position,3, PositionComp(800,800))
+table.insert(components.position,4, PositionComp(440,400))
+table.insert(components.position,5, PositionComp(140,400))
 
 table.insert(components.color, 1, ColorComp(1,0,1,1))
-table.insert(components.color, 2, ColorComp(0,1,1,1))
+table.insert(components.color, 2, ColorComp(0,1,0,1))
+table.insert(components.color, 4, ColorComp(1,0,0,1))
+table.insert(components.color, 5, ColorComp(1,0.4,1,1))
 
-local entts = {1,2,3}
+local entts = {1,2,3,4, 5}
 
 function hasComponent(e, component)
     local components = components[component]
@@ -30,22 +34,40 @@ function hasComponent(e, component)
     return componentExists
 end
 
+function systemMove(delta)
+    for i=1, #entts,1 do
+        pos = hasComponent(i, "position")
+
+        pos.x = pos.x + 100 * delta;
+    end
+end
+
+function systemDraw()
+    for i=1, #entts,1 do
+        pos = hasComponent(i, "position")
+        color = hasComponent(i, "color")
+
+        if(color) then
+            love.graphics.setColor(color.r, color.g, color.b, color.a)
+            love.graphics.rectangle("fill", pos.x, pos.y,100,100)
+        else
+            love.graphics.setColor(1,0,1,1)
+            love.graphics.rectangle("fill", pos.x, pos.y,100,100)
+        end
+    end
+end
+
 
 function love.load()
-
-
-    --print("-x-x-x-x-x-")
-    --for i = 1,3,1 do
-        --print(hasComp(i, "position"))
-        --print(hasComp(i, "color"))
-    --end
-    --print("-x-x-x-x-x-")
 end
 
 function love.update(dt)
+    systemMove(dt)
 end
 
 function love.draw()
+
+    systemDraw()
 
     --for i,compTable in pairs(components) do
         --for k,comp in pairs(compTable) do
